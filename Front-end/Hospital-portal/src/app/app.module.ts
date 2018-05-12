fd29d4085bb36f4df6156afbcb866c8f2f2f467f
hoanglong0505 <35416985+hoanglong0505@users.noreply.github.com>
2018-05-12 18:49:00 +0700

    Revert "- Merge"
    This reverts commit 57c4d480d948c09a01e6947c536c96791ae3d95b.

--- Front-end/Hospital-portal/src/app/app.module.ts
+++ Front-end/Hospital-portal/src/app/app.module.ts
@@ -21,8 +21,6 @@ import { UserDetailComponent } from './main/user/user-detail.component';
 import { RoleService } from './main/role/service/role.service';
 import { DepartmentListComponent } from './main/department/department-list.component';
 import { DepartmentDetailComponent } from './main/department/department-detail.component';
-import { CategoryListComponent } from './main/category/category-list.component';
-import { CategoryService } from './main/category/service/category.service';
 import { RoomListComponent } from './main/room/room-list.component';
 import { RoomDetailComponent } from './main/room/room-detail.component';
 import { RoomService } from './main/room/service/room.service';
@@ -30,9 +28,6 @@ import { MissionListComponent } from './main/mission/mission-list.component';
 import { ManageMissionComponent } from './main/mission/manage-mission.component';
 import { MissionDetailComponent } from './main/mission/mission-detail.component';
 import { MissionService } from './main/mission/service/mission.service';
-import { ArticleListComponent } from './main/article/article-list.component';
-import { ArticleDetailComponent } from './main/article/article-detail.component';
-import { ArticleService } from './main/article/service/article.service';
 import { ShiftSchedulerService } from './main/shiftScheduler/service/shiftScheduler.service';
 import { ManageService } from './main/shiftScheduler/service/shiftSchedulerManage.service';
 import { ShiftSchedulerListComponent } from './main/shiftScheduler/shiftScheduler-list.component';
@@ -50,22 +45,8 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { NotificationComponentService } from './main/notification/service/notification.component.service';
 import { ManageShiftSchedulerComponent } from './main/shiftScheduler/manage-shiftSchedule.component';
-import { AdminNewsComponent } from './main/news/news.component';
-import { ManageNewsComponent } from './main/news/manage-news/manage-news.component';
-import { ManageServiceComponent } from './main/service/manage-service/manage-service.component';
-import { AdminServiceComponent } from './main/service/service.component';
-import { SettingComponent } from './main/setting/setting.component';
-import { NewsComponent } from './home/news/news.component';
-import { HomeMainComponent } from './home/home-main/home-main.component';
-import { NewsDetailComponent } from './home/news/news-detail/news-detail.component';
-import { HomeComponent } from './home/home.component';
-import { MedicalComponent } from './home/medical/medical.component';
-import { MedicalDetailComponent } from './home/medical/medical-detail/medical-detail.component';
-import { IntroduceComponent } from './home/introduce/introduce.component';
 import { ConfirmationPopoverModule } from 'angular-confirmation-popover';
 import { CheckShiftScheduleComponent } from './main/shiftScheduler/check-shiftSchedule.component';
-import { ManagecategoryComponent } from './main/managecategory/managecategory.component';
-import { AddCategoryComponent } from './main/managecategory/add-category/add-category.component';
 import { PageNotFoundComponent } from './main/404/pageNotFound.component';
 import { MeetingListComponent } from './main/meetting/meeting-list.component';
 import { ManageMeetingComponent } from './main/meetting/manage-meeting.component';
@@ -74,11 +55,9 @@ import { MeetingService } from './main/meetting/service/meeting.service';
 import { UploadShiftScheduleComponent } from './main/shiftScheduler/upload-shiftSchedule.component';
 import { ManageMeetingSchedulerComponent } from './main/meetting/manageMeeting-Scheduler.component';
 import { ManageMissionSchedulerComponent } from './main/mission/manageMission-Scheduler.component';
-import { MedicalBookingComponent } from './main/medical-booking/medical-booking.component';
-import { NotiService } from './common/notification';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home/main', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'main', component: MainComponent,
     children: [
@@ -88,7 +67,6 @@ const routes: Routes = [
       { path: 'user-detail/:id', component: UserDetailComponent },
       { path: 'department-list', component: DepartmentListComponent },
       { path: 'department-detail/:id', component: DepartmentDetailComponent },
-      { path: 'category-list', component: CategoryListComponent },
       { path: 'room-list', component: RoomListComponent },
       { path: 'room-detail/:id', component: RoomDetailComponent },
       { path: 'manage-mission', component: ManageMissionComponent },
@@ -99,41 +77,18 @@ const routes: Routes = [
       { path: 'meeting-list', component: MeetingListComponent },
       { path: 'manage-meeting', component: ManageMeetingComponent },
       { path: 'meeting-detail/:id', component: MeetingDetailComponent },
-      { path: 'article-list', component: ArticleListComponent },
-      { path: 'article-detail/:id', component: ArticleDetailComponent },
       { path: 'app-demo-schedule', component: DemoScheduleComponent },
       { path: 'employee-check', component: EmployeeCheckComponent },
       { path: 'notification-list', component: NotificationListComponent },
       { path: 'notification-detail/:id', component: NotificationDetailComponent },
       { path: 'manage-shiftSchedule', component: ManageShiftSchedulerComponent },
-      { path: 'news', component: AdminNewsComponent },
-      { path: 'manage-news/:id', component: ManageNewsComponent },
-      { path: 'manage-news', component: ManageNewsComponent },
-      { path: 'manage-service/:id', component: ManageServiceComponent },
-      { path: 'service', component: AdminServiceComponent },
-      { path: 'category', component: ManagecategoryComponent },
-      { path: 'manage-category/:id', component: AddCategoryComponent },
-      { path: 'manage-category', component: AddCategoryComponent },
-      { path: 'setting', component: SettingComponent },
       { path: 'check-shiftSchedule', component: CheckShiftScheduleComponent },
       { path: 'upload-shiftSchedule', component: UploadShiftScheduleComponent },
       { path: 'manageMeeting-Scheduler', component: ManageMeetingSchedulerComponent },
-      { path: 'manageMission-Scheduler', component: ManageMissionSchedulerComponent },
-      { path: 'medical-booking', component: MedicalBookingComponent }
+      { path: 'manageMission-Scheduler', component: ManageMissionSchedulerComponent }
     ]
   },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' },  
-  {
-    path: 'home', component: HomeComponent,
-    children: [
-      { path: 'news/:id', component: NewsComponent },
-      { path: 'news-detail/:id', component: NewsDetailComponent },
-      { path: 'medical-booking', component: MedicalComponent },
-      { path: 'medical-detail', component: MedicalDetailComponent },
-      { path: 'main', component: HomeMainComponent },
-      { path: 'introduce/:id', component: IntroduceComponent },
-    ]
-  },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
 ];
 
@@ -147,7 +102,6 @@ const routes: Routes = [
     UserDetailComponent,
     DepartmentListComponent,
     DepartmentDetailComponent,
-    CategoryListComponent,
     RoomListComponent,
     RoomDetailComponent,
     MissionListComponent,
@@ -158,8 +112,6 @@ const routes: Routes = [
     MeetingListComponent,
     MeetingDetailComponent,
     ManageMeetingComponent,
-    ArticleListComponent,
-    ArticleDetailComponent,
     DemoScheduleComponent,
     EmployeeCheckComponent,
 
@@ -168,26 +120,11 @@ const routes: Routes = [
     NotificationDetailComponent,
     ManageShiftSchedulerComponent,
 
-    HomeComponent,
-    MedicalComponent,
-    MedicalDetailComponent,
-    NewsComponent,
-    HomeMainComponent,
-    AdminNewsComponent,
-    NewsDetailComponent,
-    ManageNewsComponent,
-    AdminServiceComponent,
-    ManageServiceComponent,
-    SettingComponent,
-    IntroduceComponent,
     PageNotFoundComponent,
     UploadShiftScheduleComponent,
     CheckShiftScheduleComponent,
-    ManagecategoryComponent,
-    AddCategoryComponent,
     ManageMeetingSchedulerComponent,
-    ManageMissionSchedulerComponent,
-    MedicalBookingComponent
+    ManageMissionSchedulerComponent
   ],
   imports: [
     BrowserModule,
@@ -207,9 +144,8 @@ const routes: Routes = [
   ],
   providers: [
     NotificationService, LoadingService, LoginService, AccountService, ApiService, UserService,
-    DepartmentService, RoleService, CategoryService, RoomService, MissionService,
-    ShiftSchedulerService, ManageService, MeetingService, ArticleService, CookieService, SelectService, NotificationComponentService
-    , NotiService
+    DepartmentService, RoleService, RoomService, MissionService,
+    ShiftSchedulerService, ManageService, MeetingService, CookieService, SelectService, NotificationComponentService
   ],
   bootstrap: [AppComponent]
 })
